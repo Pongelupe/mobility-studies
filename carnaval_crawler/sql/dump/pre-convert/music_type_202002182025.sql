9774c972e594b3455990d7e77a1510912549f428
Pongelupe <pongelupe1998@gmail.com>
2020-02-18 21:40:21 -0300

    adding dumps

---
+++ carnaval_crawler/sql/dump/pre-convert/music_type_202002182025.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.music_type (desc_music_type) VALUES 
+('FUNK')
+,('ROCK')
+,('MPB')
+,('FORRÓ')
+,('SAMBA')
+,('POP')
+,('MARCHINHA')
+,('AXÉ')
+,('JAZZ')
+,('PAGODE')
+;
+INSERT INTO public.music_type (desc_music_type) VALUES 
+('SERTANEJO')
+,('OUTROS')
+,('AFRO')
+;
---
+++ carnaval_crawler/sql/dump/pre-convert/profile_202002182025.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.profile (desc_profile) VALUES 
+('ADULTO')
+,('INFANTIL')
+,('PET')
+,('TERCEIRA IDADE')
+,('RELIGIOSO')
+,('LGBT')
+;
