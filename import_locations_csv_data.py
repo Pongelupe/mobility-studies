1fa66ced151f009fce4a7696a5c5d258fee7b13b
Pongelupe <pongelupe1998@gmail.com>
2020-02-20 19:59:03 -0300

    adding requirements.txt

--- import_locations_csv_data.py
+++ import_locations_csv_data.py
@@ -1,25 +1,29 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python:
-import csv23 
+
+import csv 
 import psycopg2
 
-with csv23.open_reader('data/PessoasFrequentamConenxaoEoutrosLocais2.csv') as csv_file:
+with open('data/PessoasFrequentamConenxaoEoutrosLocais2.csv') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
     con = psycopg2.connect(host='localhost', port=25432, database='mob',
             user='mob', password='mob')
     cursor = con.cursor()
 
     query = """
-    SELECT COUNT(*) FROM public.conexao
+    SELECT COUNT(*) FROM conexao
     WHERE mac IN %s;
     """
 
     line = 0
     macs = set()
-    for p in csv_file:
+    for p in reader:
         if p[0].startswith( 'Conexão' ) and line !=0:
             macs.add(p[1])
         line += 1
     print(tuple(macs))
-    print(cursor.execute(query, (tuple(macs), )))
+    cursor.execute(query, (tuple(macs), ))
+    print(f"{cursor.fetchone()[0]} conections at Conexao and other places")
 
     con.commit()
     cursor.close()
