bbc904ce30019c6f6a912a5092a84cccb5ce703e
Pongelupe <pongelupe1998@gmail.com>
2020-02-22 19:15:12 -0300

    adding twitter crawler

---
+++ carnaval_crawler/twitter_data/carnaval_data_scraping.py
@@ -0,0 +1,50 @@
+import json
+import tweepy
+import psycopg2
+import pymongo
+
+#------------- SQL QUERIES
+INSERT_SEARCH = "INSERT INTO public.tweet_search_control (id_block, initial_search_time) VALUES(%s, current_timestamp) RETURNING id_search"
+INSERT_TWEET = "INSERT INTO public.tweet (id_tweet, truncated) VALUES(%s, %s)" 
+INSERT_TWEETxSEARCH = "INSERT INTO public.tweetxsearch (id_tweet, id_search) VALUES(%s, %s)" 
+EXISTS_TWEET = "SELECT COUNT(1) > 0 FROM public.tweet WHERE id_tweet = %s"
+UPDATE_SEARCH = "UPDATE public.tweet_search_control SET final_search_time= current_timestamp WHERE id_search = %s "
+#------------- SQL QUERIES
+
+def job(id_block, newest_tweet_id, search_query, auth):
+#------------- DATABASES
+    con = psycopg2.connect(host='localhost', port=25432, database='mob',
+            user='mob', password='mob')
+    cursor = con.cursor()
+
+    mongo_client = pymongo.MongoClient('mongodb://localhost:27017')
+    tweets_collection = mongo_client['carnaval_db']['tweets']
+#------------- DATABASES
+
+    cursor.execute(INSERT_SEARCH, (id_block, ))
+    search_id = cursor.fetchone()[0]
+
+    api = tweepy.API(auth)
+    public_tweets = tweepy.Cursor(api.search, q=search_query).items(1000) if newest_tweet_id == -1 else tweepy.Cursor(api.search, q=search_query, since_id = newest_tweet_id).items(1000)
+    print(public_tweets)
+    #print(f"searched {len(list(public_tweets))} tweets!")
+    for tweet in public_tweets:
+        tweet_id = tweet._json['id']
+        cursor.execute(EXISTS_TWEET, (tweet_id, ))
+        exists = cursor.fetchone()[0]
+        if not exists: #new tweet!
+            truncated = tweet._json['truncated']
+            cursor.execute(INSERT_TWEET, (tweet_id, truncated, ))
+            if not truncated:
+                tweets_collection.insert_one(tweet._json)
+                print('inserted on mongo')
+            else: print('truncated!')
+        cursor.execute(INSERT_TWEETxSEARCH, (tweet_id, search_id, ))
+        con.commit()
+
+    cursor.execute(UPDATE_SEARCH, (search_id, ))
+    con.commit()
+
+    cursor.close()
+    mongo_client.close()
+    print("job done")
---
+++ carnaval_crawler/twitter_data/carnaval_scheduler.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import psycopg2
+import schedule
+import time
+import tweepy
+import sys
+import carnaval_data_scraping
+
+consumer_key = sys.argv[1] # Twitter consumer key
+consumer_secret = sys.argv[2] # Twitter consumer secret
+access_token = sys.argv[3] # Twitter access token
+access_token_secret = sys.argv[4] # Twitter access token secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+con = psycopg2.connect(host='localhost', port=25432, database='mob',
+            user='mob', password='mob')
+cursor = con.cursor()
+
+SELECT_ID_BLOCK_FIRST_TIME = """
+SELECT id_block FROM carnival_block cb
+	WHERE cb.id_block NOT IN (SELECT id_block FROM tweet_search_control)
+	ORDER BY cb.id_block LIMIT 1;
+"""
+SELECT_ID_BLOCK_LATER = """
+select
+	id_block
+from
+	(
+	select
+		tsc.id_block,
+		count(txs.*) as c
+	from
+		tweet_search_control tsc
+	join carnival_block cb on
+		cb.id_block = tsc.id_block
+	join tweetxsearch txs on
+		txs.id_search = tsc.id_search
+	where
+		cb."date" < current_timestamp
+		and tsc.final_search_time is not null
+	group by
+		1
+	order by
+		2) as ids_and_tweet_count
+where ids_and_tweet_count.id_block in (select tsc2.id_block from tweet_search_control tsc2
+where tsc2.final_search_time > current_timestamp - interval '4 hours'
+and tsc2.final_search_time = (select max(tsc3.final_search_time) from tweet_search_control tsc3 where tsc3.id_block = ids_and_tweet_count.id_block limit 1))
+order by ids_and_tweet_count.c
+limit 1;
+"""
+SELECT_NEWEST_TWEET_BY_BLOCK_ID = """
+select max(txs.id_tweet) from tweetxsearch txs
+join tweet_search_control tsc on tsc.id_search = txs.id_search
+where tsc.id_block = %s
+"""
+SELECT_BLOCK_NAME_BY_BLOCK_ID = 'select "name" from carnival_block where id_block = %s'
+
+def get_block_id():
+    cursor.execute(SELECT_ID_BLOCK_FIRST_TIME)
+    block = cursor.fetchone()
+    if block == None: # all blocks have been scraped at least once!
+        cursor.execute(SELECT_ID_BLOCK_LATER)
+        return cursor.fetchone()[0]
+    else:
+        return block[0]
+
+def get_newest_tweet_by_block_id(block_id):
+    cursor.execute(SELECT_NEWEST_TWEET_BY_BLOCK_ID, (block_id, ))
+    newest = cursor.fetchone()
+    return -1 if newest[0] == None else newest[0]
+
+def get_search_query_block_id(block_id):
+    cursor.execute(SELECT_BLOCK_NAME_BY_BLOCK_ID, (block_id, ))
+    name = cursor.fetchone()[0]
+    if name.startswith('BLOCO') and len(name.split()) == 2:
+        return f'"{name}" OR {name.split()[1]}'
+    else: return name
+
+def job(block, newest_tweet, search_query, auth):
+    block_id = block()
+    newest_tweet_id = newest_tweet(block_id)
+    search_query_text = search_query(block_id)
+    print(f"{block_id},{newest_tweet_id},{search_query_text}")
+    carnaval_data_scraping.job(block_id, newest_tweet_id, search_query_text, auth)
+
+schedule.every(1).minutes.do(job, lambda : get_block_id(), lambda block_id :get_newest_tweet_by_block_id(block_id), lambda block_id : get_search_query_block_id(block_id), auth)
+
+#execute at first
+job(lambda : get_block_id(), lambda block_id :get_newest_tweet_by_block_id(block_id), lambda block_id : get_search_query_block_id(block_id), auth)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
