a7634b3248526d7d91b61d3e5d8d3c37060bcf14
Pongelupe <pongelupe1998@gmail.com>
2020-03-14 17:09:13 -0300

    now retreiving data from the seven first reviews

--- google_places_searcher/review_spider/review_spider.py
+++ google_places_searcher/review_spider/review_spider.py
@@ -1,11 +1,14 @@
 import scrapy
+import base64
 from scrapy_splash import SplashRequest
 
 class ReviewSpider(scrapy.Spider):
     
     script_click_a = """
     function main(splash, args)
+        assert(splash:go(args.url))
         splash:runjs("document.querySelector('#rhs > div > div.kp-blk.knowledge-panel.Wnoohf.OJXvsb > div > div.ifM9O > div > div.kp-header > div:nth-child(2) > div.fYOrjf.iB08Xb.kp-hc > div:nth-child(2) > div > div > span.hqzQac > span > a').click()")
+        assert(splash:wait(0.75))
         return splash:html()
     end
     """
@@ -20,20 +23,35 @@ class ReviewSpider(scrapy.Spider):
             yield SplashRequest(url, self.parse,
                 endpoint='render.html',
                 args={'wait': 0.5},
+                meta={'original_url': url}
             )
     def parse(self, response):
         comments = response.xpath('//*[@id="rhs"]/div/div[1]/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div/div/span[2]/span/a/span/text()').get()
         if comments:
             name = response.xpath('//*[@id="rhs"]/div/div[1]/div/div[1]/div/div[1]/div[2]/div[2]/div[1]/div/div[2]/div[1]/span/text()').get()
             print(f'{comments} for {name}')
-            url = response.request.url
+            comments_count = int(comments.split()[0].replace('.', ''))
+            url = response.meta['original_url']
             yield SplashRequest(url, self.parse_result,
                     endpoint='execute',
+                    dont_filter = True,
                     args={'lua_source': self.script_click_a,
                         'url': url},
+                    meta={'reviews_count': comments_count}
                     )
         else:
             yield {}
 
     def parse_result(self, response):
-        print(response.body)
+        review_divs = response.css('.WMbnJf').extract()
+
+        reviewrs = response.xpath('//div[@class="TSUbDb"]/a')
+        comment_count = response.xpath('//a[@class="Msppse"]/span/text()').getall()
+        stars_and_relative_time = response.xpath('//div[@class="PuaHbe"]')
+        reviews = response.xpath('//div[@class="Jtu6Td"]/span')
+
+        for i,r in enumerate(review_divs):
+            hidden_review = reviews[i].xpath('.//span[@style="display:none"]/text()').get() 
+            review = hidden_review if hidden_review is not None else reviews[i].xpath('.//text()').get()
+            print(f"""{i} --> {reviewrs[i].attrib['href']}, {comment_count[i]}, {stars_and_relative_time[i].xpath('.//span[@class="dehysf"]/text()').get()}, {comment_count[i]}, {stars_and_relative_time[i].xpath('.//g-review-stars/span/span').attrib['style'].split(':')[1][:-2]}, {review}""")
+
